be83da08efa95ff0db74fdd989d12e9fc2bab8e6
Dan Jackson <danjackson@hastingsdirect.com>
2020-02-27 12:17:40 +0000

    The Button component moved into a subdirectory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Button from './components/Button';
+import Button from './components/Button/Button';
 
 class App extends Component {
 	render() {
