928d7456bd0ffd1cfe43f1e4fd8a713f8f2ac2d8
Dan Jackson <danjackson@hastingsdirect.com>
2020-02-27 12:37:47 +0000

    Semi colons added to the end of statements within the Button component test file

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Button from './Button'
+import React from 'react';
+import { shallow } from 'enzyme';
+import Button from './Button';
 
 describe('Button', () => {
     it('Button renders correctly', () => {
