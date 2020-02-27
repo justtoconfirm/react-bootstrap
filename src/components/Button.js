07a8667ca219dbcd8a58209042c000764c5cb636
Dan Jackson <danjackson@hastingsdirect.com>
2020-02-27 08:28:02 +0000

    Props added to the Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
-const Button = () => {
+const Button = (props) => {
+
+	const { type = 'button',
+			url = '',
+			children
+	} = props;
+
+	const isAnchor = url && (url.includes('http') || url.startsWith('#') || url.startsWith('mailto') || url.startsWith('/'));
+
+	const renderAnchor = () =>
+		<a href={url} role="button">{children}</a>
+
+	const renderButton = () =>
+		<button type={type}>{children}</button>
+
 	return (
-		<button>Button</button>
+		isAnchor ? renderAnchor() : renderButton()
 	)
 }
 
