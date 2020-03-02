2dd41f4e6e71fccae7702e100972661259f63bbc
Dan Jackson <danjackson@hastingsdirect.com>
2020-03-02 17:02:35 +0000

    Syntax revisions within the Storybook config file

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,8 @@
-import { configure } from "@storybook/react";
+import { configure } from '@storybook/react'
 
 function loadStories() {
   // You can require as many stories as you need.
   require('./../src/components/Button/storybook/Button.stories.js')
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
