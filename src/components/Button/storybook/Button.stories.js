6e5dccb3700f1c35230da73918813ffffa3e176d
Dan Jackson <danjackson@hastingsdirect.com>
2020-03-02 16:58:20 +0000

    Linter run over the Button stories JavaScript file

--- src/components/Button/storybook/Button.stories.js
+++ src/components/Button/storybook/Button.stories.js
@@ -3,9 +3,9 @@ import { storiesOf } from '@storybook/react'
 import Button from './../Button'
 
 storiesOf('Button', module)
-	.add('with text', () => (
-		<Button type='button'>Button</Button>
-	))
-	.add('as a link', () => (
-		<Button url='http://example.com'>Link</Button>
-	))
+  .add('with text', () => (
+    <Button type='button'>Button</Button>
+  ))
+  .add('as a link', () => (
+    <Button url='http://example.com'>Link</Button>
+  ))
